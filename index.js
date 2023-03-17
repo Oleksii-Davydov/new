c9a8bf9b824eaea3ad5c1c0692ecac4f13d0cb4c
Oleksii-Davydov <davydovalekcey1992@gmail.com>
2023-03-17 21:53:10 +0200

    v1

---
+++ index.js
@@ -0,0 +1,7 @@
+const firstNumber = prompt("Enter the first number")
+const secondNumber = prompt("Enter the second number")
+alert(`User entered ${firstNumber} and ${secondNumber}:  \n 
+    ${firstNumber} + ${secondNumber} = ${+firstNumber + +secondNumber};
+    ${firstNumber} - ${secondNumber} = ${+firstNumber - +secondNumber};
+    ${firstNumber} * ${secondNumber} = ${+firstNumber * +secondNumber};
+    ${firstNumber} / ${secondNumber} = ${+firstNumber / +secondNumber}`)
